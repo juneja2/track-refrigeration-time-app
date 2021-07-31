b07e99637831702365ac11ec8c23125cecb19add
Parshant Juneja <juneja2019@berkeley.edu>
2021-07-31 15:38:28 -0700

    Finished the update and get out time functionality. Need to test the app

--- app/controllers/refrigeration_time_controller.rb
+++ app/controllers/refrigeration_time_controller.rb
@@ -3,8 +3,66 @@ class RefrigerationTimeController < ApplicationController
   end
 
   def updateOutTime
+    updateParams = updateOutTimeParams
+    item_id, location_id, timestamp = updateParams[:item_id], updateParams[:location_id], updateParams[:timestamp]
+
+    sql = %{
+      SELECT * 
+      FROM cache_tables 
+      WHERE item_id = #{item_id}
+      LIMIT 1
+    }
+
+    # Queries
+    cache_table_record = ActiveRecord::Base.connection.execute sql
+    curr_location = Location.find(location_id)
+
+    if cache_table_record
+      new_time_outside = cache_table_record[:time_outside] 
+      unless cache_table_record[:prev_loc_is_freezer]
+        new_time_outside += (timestamp - cache_table_record[:last_time_stamp])
+      end
+
+      sql = %{
+        UPDATE cache_tables
+        SET time_outside        = #{new_time_outside}
+        SET prev_loc_is_freezer = #{curr_location[:is_freezer]}
+        SET last_time_stamp     = #{timestamp}
+        WHERE item_id           = #{item_id}
+      }
+    else
+      # If you don't have a cache_record => you are in a refrigerator
+      new_time_outside = 0
+      sql = %{
+        INSERT INTO cache_tables (#{item_id}, #{new_time_outside}, #{curr_location[:is_freezer]}, #{timestamp})
+      }
+    end
+
+    ActiveRecord::Base.connection.execute sql
+    redirect_to get_out_time_path
   end
 
   def getOutTime
+    @item_id = getOutTimeParams[:item_id]
+
+    sql = %{
+      SELECT * 
+      FROM cache_tables 
+      WHERE item_id = #{item_id}
+      LIMIT 1
+    }
+
+    # Queries
+    cache_table_record = ActiveRecord::Base.connection.execute sql
+    @outTime = cache_table_record ? cache_table_record[:time_outside] : 0
   end
+
+  private
+    def updateOutTimeParams
+      params.permit(:item_id, :location_id, :timestamp)
+    end
+    
+    def getOutTimeParams
+      params.permit(:item_id)
+    end
 end
