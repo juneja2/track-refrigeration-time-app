951e1d45a83a801735dc9c31a89b828f53db4b15
Parshant Juneja <juneja2019@berkeley.edu>
2021-07-31 23:28:37 -0700

    Database seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+for i in 0..5
+  Location.create(is_freezer: true)
+end
+
+for i in 0..5
+  Location.create(is_freezer: false)
+end
